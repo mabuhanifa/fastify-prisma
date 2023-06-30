f7863caf4d752ec6eaab98d062bdedbc1f06938c
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-01 03:44:10 +0600

    /healthcheck

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-console.log("object");
+import Fastify from "fastify";
+const server = Fastify();
+const port = Number(process.env.PORT) || 3000;
+
+server.get("/healthcheck", async (request, response) => {
+  return { status: "ok" };
+});
+
+async function main() {
+  try {
+    server.get("/", async (request, response) => {
+      return { hello: "world" };
+    });
+
+    await server.listen(3000, "0.0.0.0");
+    console.log(`Server listening at ${port}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+main();
